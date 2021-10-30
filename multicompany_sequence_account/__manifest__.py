ee3ffddb6e61d15a4ed4a94dc270bd68be387603
Tuga Yousif <tugakariem@gmail.com>
2021-10-30 18:59:23 +0200

    Add files via upload

---
+++ multicompany_sequence_account/__manifest__.py
@@ -0,0 +1,12 @@
+
+
+{
+    "name": "Multi-company sequence Account",
+    "depends": ["account","multicompany_sequence"],
+    "data": [
+
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+}
